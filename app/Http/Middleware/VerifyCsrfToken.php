8353eebfa52bd1b5d65c8494dc006a5ecbedc063
toby <tkloespik@gmail.com>
2022-01-22 00:40:04 +0800

    opt

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -13,6 +13,6 @@ class VerifyCsrfToken extends Middleware
      */
     protected $except = [
         //
-        'make-payment','receiveNotify','/','success','c-notify','creditCard'
+        'creditCard/receiveNotify','creditCard/success','/','success','receiveNotify','creditCard'
     ];
 }
